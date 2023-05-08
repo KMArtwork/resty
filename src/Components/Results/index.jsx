f65164e9e9d53d8f14095aa641b8f269f92ac963
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-05-08 13:48:56 -0700

    add .scss files for each component

--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './Results.scss';
+
 function Results (props) {
   return (
     <section>
-      <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      <pre>
+        {props.data ? JSON.stringify(props.data, undefined, 2) : null}
+      </pre>
     </section>
   );
 }
